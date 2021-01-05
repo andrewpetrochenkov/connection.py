a9aeb00262162f1f1943d6e6b7dee61397113a2a
andrewp-as-is <russianidiot.github@gmail.com>
2021-01-05 11:59:57 +0300

    2021:01:05 11:59

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='connection',
-    version='2020.12.3',
+    version='2021.1.5',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
